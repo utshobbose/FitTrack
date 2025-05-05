387b63e62504a0dc1c1db005499b4853c884e03b
utshobbose <utshob.bose@g.bracu.ac.bd>
2025-05-05 22:33:00 +0600

    Update api.jsx

--- frontend/src/context/api.jsx
+++ frontend/src/context/api.jsx
@@ -1,1 +1,2 @@
-export const backend =  "http://localhost:5000/api" || "https://fittrack-y3la.onrender.com/api";
+// export const backend =  "http://:5000/api" || "https://fittrack-y3la.onrender.com/api";
+export const backend = import.meta.env.VITE_BACKEND_URL
